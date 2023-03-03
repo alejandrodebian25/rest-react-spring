2e71d31dc7447e756efb2a0a2eed99909d248d7c
Alejandro Quea <alejandrodebian25@gmail.com>
2023-03-02 22:22:31 -0400

    Inicio backend

---
+++ rest-backend/src/main/java/com/adev/rest/backend/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.adev.rest.backend;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RestBackendApplication.class);
+	}
+
+}
