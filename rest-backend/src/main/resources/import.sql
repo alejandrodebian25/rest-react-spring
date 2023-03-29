11260f6f8b5b3a579ac49d99a027fc265322f9a8
Alejandro Quea <alejandrodebian25@gmail.com>
2023-03-28 21:40:05 -0400

    Adicion en import

--- rest-backend/src/main/resources/import.sql
+++ rest-backend/src/main/resources/import.sql
@@ -1,1 +1,4 @@
-INSERT INTO public.libro (descripcion, nro_paginas, titulo)VALUES('Descripcion 1', 222, 'Libro 1');
+INSERT INTO public.autor (nombre, apellido, edad,nacionalidad)VALUES('Alejandro','Quea', 22, 'Boliviano');
+
+INSERT INTO public.libro (descripcion, nro_paginas, titulo)VALUES('Descripcion 1', 222, 'Libro 1',1);
+
